c9f2e674692397b84c780e63735dfd373fd4f3df
Arek Benedykt <arkadiusz@benedykt.net>
2013-05-20 23:35:33 +0200

    initial commit

---
+++ SignaleRDemo/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
+using SignaleRDemo.Hubs;
+
+namespace SignaleRDemo
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
+
+            new TaskFactory().StartNew(() =>
+            {
+                while (true)
+                {
+                    var context = GlobalHost.ConnectionManager.GetHubContext<DemoHub>();
+                    context.Clients.All.ping("from code", DateTime.Now.ToLongTimeString());
+                    Thread.Sleep(1000);
+                }
+
+            });
+
+            new TaskFactory().StartNew(() =>
+            {
+                while (true)
+                {
+                    var context = GlobalHost.ConnectionManager.GetHubContext<DemoHub>();
+                    var sensorId = new Random(DateTime.Now.Millisecond).Next(9) + 1;
+                    context.Clients.All.sensor(sensorId);
+                    Thread.Sleep(1000);
+                }
+
+            });
+
+        }
+    }
+}
---
+++ SignaleRDemo/Hubs/DemoHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignaleRDemo.Hubs
+{
+    
+    public class DemoHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.ping(name, message);
+        }
+    }
+}
